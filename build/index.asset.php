c076425d880b1f394e2a0f1f23933871d48900d2
jdhall5 <jhall5@highpoint.edu>
2024-01-16 10:25:32 -0500

    refactored sass variables to main style file and imported to other files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-dom-ready'), 'version' => '72cccad6c14335f9d9f9');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-dom-ready'), 'version' => '5ba755f6b571ccf0f2bb');

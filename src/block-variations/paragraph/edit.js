6bf01f883d27a6dd40f5ce712703448c5ddda8e5
Flores <rflores1@highpoint.edu>
2024-07-29 20:26:15 -0400

    formating edit js file

--- src/block-variations/paragraph/edit.js
+++ src/block-variations/paragraph/edit.js
@@ -1,37 +1,76 @@
-import { InspectorControls, RichText } from '@wordpress/block-editor';
-import { Panel, PanelBody, ButtonGroup, Button, ToggleControl } from '@wordpress/components';
-import { Component } from '@wordpress/element';
-// import StyleSelector from '../../style-selector';
-import { __experimentalHeading as Heading } from '@wordpress/components';
-import '../../style.scss';
-
-//add bold and italic to allowed formats and buttons
+import { InspectorControls, RichText } from "@wordpress/block-editor";
+import {
+	Panel,
+	PanelBody,
+	ButtonGroup,
+	Button,
+	ToggleControl,
+} from "@wordpress/components";
+import { Component } from "@wordpress/element";
+import { __experimentalHeading as Heading } from "@wordpress/components";
+import "../../style.scss";
 
 export default class HPUEditParagraph extends Component {
-    render() {
-        const { attributes, setAttributes } = this.props;
-        console.log(attributes)
+	render() {
+		const { attributes, setAttributes } = this.props;
 
-        return <div>
-            <InspectorControls>
-                <Panel header="HPU Blocks Paragraph Settings" className='hpu-blocks-emphasis-style'>
-                    <PanelBody title="Text Settings">
-                        <Heading>Alignment</Heading>
-                        <ButtonGroup>
-                            <Button isPrimary={attributes.align === 'left'} onClick={() => setAttributes({ align: 'left' })}>Left</Button>
-                            <Button isPrimary={attributes.align === 'center'} onClick={() => setAttributes({ align: 'center' })}>Center</Button>
-                            <Button isPrimary={attributes.align === 'right'} onClick={() => setAttributes({ align: 'right' })}>Right</Button>
-                            <Button isPrimary={attributes.align === 'justify'} onClick={() => setAttributes({ align: 'justify' })}>Justify</Button>
-                        </ButtonGroup>
-                    </PanelBody>
-                </Panel>
-            </InspectorControls>
-            <RichText
-                tagName="p"
-                className={[attributes.styleClass, (attributes.dropCap ? 'has-drop-cap' : ''), (attributes.align ? `align-${attributes.align}` : '')].join(' ')}
-                value={attributes.content}
-                onChange={(content) => setAttributes({ content })}
-            />
-        </div>;
-    }
+		return (
+			<div>
+				<InspectorControls>
+					<Panel
+						header="HPU Blocks Paragraph Settings"
+						className="hpu-blocks-emphasis-style"
+					>
+						<PanelBody title="Text Settings">
+							<Heading>Alignment</Heading>
+							<ButtonGroup>
+								<Button
+									isPrimary={ attributes.align === 'left' }
+									onClick={ () =>
+										setAttributes( { align: 'left' } )
+									}
+								>
+									Left
+								</Button>
+								<Button
+									isPrimary={ attributes.align === 'center'}
+									onClick={ () =>
+										setAttributes( { align: 'center' } )
+									}
+								>
+									Center
+								</Button>
+								<Button
+									isPrimary={ attributes.align === 'right' }
+									onClick={ () =>
+										setAttributes( { align: 'right' } )
+								}
+								>
+									Right
+								</Button>
+								<Button
+									isPrimary={ attributes.align === 'justify' }
+									onClick={ () =>
+										setAttributes( { align: 'justify' } )
+									}
+								>
+									Justify
+								</Button>
+							</ButtonGroup>
+						</PanelBody>
+					</Panel>
+				</InspectorControls>
+				<RichText
+					tagName="p"
+					className={[
+						attributes.styleClass,
+						attributes.dropCap ? 'has-drop-cap' : '',
+						attributes.align ? `align-${ attributes.align }` : '',
+					].join( ' ' ) }
+					value={ attributes.content }
+					onChange={ ( content ) => setAttributes( { content } ) }
+				/>
+			</div>
+		);
+	}
 }
